70b8834c375023f1802f6695557a64b9b90f7031
DESKTOP-0ICLQS4\enz <vincenzo.orefice@eng.it>
2021-03-01 20:47:24 +0100

    Add Repository into Controller

--- src/main/java/io/mosfet/scraperone/web/AvailabilityController.java
+++ src/main/java/io/mosfet/scraperone/web/AvailabilityController.java
@@ -1,14 +1,26 @@
 package io.mosfet.scraperone.web;
 
+import io.mosfet.scraperone.port.AvailabilityRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AvailabilityController {
-    @GetMapping("/availability")
-    public boolean getAvailability() {
+    private final AvailabilityRepository availabilityRepository;
 
-        return true;
+    public AvailabilityController(AvailabilityRepository availabilityRepository) {
+
+        this.availabilityRepository = availabilityRepository;
+    }
+
+    @GetMapping("/availability")
+    public ResponseEntity<Boolean> getAvailability() {
+
+        if (availabilityRepository.retrieveAvailability().isAvailable()) {
+            return ResponseEntity.ok(true);
+        }
+       return ResponseEntity.status(400).body(false);
     }
 
 }
--- src/test/java/io/mosfet/scraperone/web/AvailabilityControllerTest.java
+++ src/test/java/io/mosfet/scraperone/web/AvailabilityControllerTest.java
@@ -1,7 +1,11 @@
 package io.mosfet.scraperone.web;
 
+import io.mosfet.scraperone.domain.PlaystationAvailability;
+import io.mosfet.scraperone.port.AvailabilityRepository;
 import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -11,13 +15,35 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 class AvailabilityControllerTest {
+    @Mock
+    private AvailabilityRepository availabilityRepository;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+    }
+
     @Test
     void isSuccessful() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new AvailabilityController()).build();
+        when(availabilityRepository.retrieveAvailability()).thenReturn(new PlaystationAvailability(true));
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new AvailabilityController(availabilityRepository)).build();
         mockMvc.perform(MockMvcRequestBuilders.get(new URIBuilder("/availability").build())
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.content().string("true"));
+                .andExpect(MockMvcResultMatchers.content().string("true"));
+    }
+
+    @Test
+    void is4xx() throws Exception {
+        when(availabilityRepository.retrieveAvailability()).thenReturn(new PlaystationAvailability(false));
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new AvailabilityController(availabilityRepository)).build();
+        mockMvc.perform(MockMvcRequestBuilders.get(new URIBuilder("/availability").build())
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.content().string("false"));
     }
 }
